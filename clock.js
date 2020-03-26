9527e0cf12fb197870c30c23550044973c3d54a5
wisehero <54886222+wisehero@users.noreply.github.com>
2020-03-26 23:47:16 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,6 +1,8 @@
+//시간 표시하는 요소 선택
 const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
+// 현재 시각을 얻는 함수
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
@@ -11,6 +13,7 @@ function getTime() {
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
+//실행 함수
 function init() {
   getTime();
   setInterval(getTime, 1000); //setInterval(함수, 실행간격시간)
